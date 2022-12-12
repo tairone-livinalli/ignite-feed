c130d765a384d6d117fad8d81e7a1e5d20ca584c
Tairone Livinalli <taironelivinalli@hotmail.com>
2022-12-12 11:39:38 -0500

    chore: add @next/font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
 import '../styles/globals.ts';
 import type { AppProps } from 'next/app';
+import { Roboto_Slab } from '@next/font/google';
+
+const robotoSlab = Roboto_Slab();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${robotoSlab.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 }
