46dbd33a6101e437b75d68e0c7c857a42e2626e0
Tairone Livinalli <taironelivinalli@hotmail.com>
2022-12-13 16:55:35 -0500

    refactor: move global styles and add navbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import '../styles/globals.ts';
 import type { AppProps } from 'next/app';
 import { Roboto_Slab } from '@next/font/google';
+import Head from 'next/head';
+import Navbar from '../components/navbar/navbarComponent';
 
 const robotoSlab = Roboto_Slab();
 
@@ -11,7 +13,45 @@ export default function App({ Component, pageProps }: AppProps) {
         html {
           font-family: ${robotoSlab.style.fontFamily};
         }
+        * {
+          margin: 0;
+          padding: 0;
+          box-sizing: border-box;
+          outline: 0;
+        }
+        body {
+          background: #121214;
+          color: #fff;
+          -webkit-font-smoothing: antialiased;
+        }
+        body,
+        input,
+        button {
+          font-family: 'Roboto Slab', serif;
+          font-size: 16px;
+        }
+        h1,
+        h2,
+        h3,
+        h4,
+        h5,
+        h6,
+        strong {
+          font-weight: 500;
+        }
+        button {
+          cursor: pointer;
+        }
       `}</style>
+      <Head>
+        <title>TaiMedia</title>
+        <meta
+          name="description"
+          content="Kind of a social media to play around"
+        />
+        <link rel="icon" href="/icon.svg" />
+      </Head>
+      <Navbar />
       <Component {...pageProps} />
     </>
   );
