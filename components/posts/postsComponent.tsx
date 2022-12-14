4d101406106b3ddd0fb1f560bfd884d221a07bb9
Tairone Livinalli <taironelivinalli@hotmail.com>
2022-12-14 13:11:46 -0500

    refactor: change posts component to use dynamic data

--- components/posts/postsComponent.tsx
+++ components/posts/postsComponent.tsx
@@ -1,194 +1,31 @@
-import Image from 'next/image';
 import React from 'react';
-import styled from 'styled-components';
 
-const PostContainer = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
+import PostItem from './postItemComponent';
 
-  width: 100%;
-  border-radius: 8px;
-  padding: 40px;
-  gap: 20px;
+interface Owner {
+  name: string;
+  avatarUrl: string;
+  role: string;
+}
 
-  background-color: #202024;
-`;
+interface PostItem {
+  owner: Owner;
+  createdAt: Date;
+  contentText: string;
+  contentImage: string;
+}
 
-const PostTitle = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  width: 100%;
-`;
+interface PostsProps {
+  posts: PostItem[];
+}
 
-const PostOwner = styled.div`
-  display: flex;
-  gap: 16px;
-`;
-
-const PostOwnerAvatar = styled.div`
-  display: flex;
-
-  padding: 4px;
-  border: 2px solid #00b37e;
-  border-radius: 8px;
-`;
-
-const PostOwnerInfo = styled.div`
-  display: flex;
-  align-items: flex-start;
-  justify-content: center;
-  flex-direction: column;
-  gap: 4px;
-`;
-
-const PostOwnerName = styled.p`
-  font-size: 16px;
-  font-weight: 700;
-  color: #e1e1e6;
-`;
-
-const PostOwnerRole = styled.p`
-  font-size: 14px;
-  color: #8d8d99;
-`;
-
-const PostInfo = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
-const PostInfoTime = styled.p`
-  font-size: 14px;
-  color: #8d8d99;
-`;
-
-const PostContent = styled.div`
-  display: flex;
-  align-items: flex-start;
-  flex-direction: column;
-  gap: 12px;
-`;
-
-const PostContentText = styled.p`
-  font-size: 16px;
-  color: #c4c4cc;
-`;
-
-const Divider = styled.div`
-  width: 100%;
-  height: 0px;
-
-  border: 1px solid #323238;
-`;
-
-const Form = styled.form`
-  display: flex;
-  align-items: flex-start;
-  justify-content: center;
-  flex-direction: column;
-  gap: 12px;
-
-  width: 100%;
-`;
-
-const FormLabel = styled.label`
-  display: flex;
-  align-items: flex-start;
-  flex-direction: column;
-  gap: 20px;
-
-  width: 100%;
-`;
-
-const FormSpan = styled.span`
-  font-size: 16px;
-  font-weight: 700;
-  color: #e1e1e6;
-`;
-
-const FormTextArea = styled.textarea`
-  width: 100%;
-  padding: 8px;
-
-  background-color: #121214;
-  border-radius: 8px;
-
-  font-size: 16px;
-  color: #c4c4cc;
-
-  :hover,
-  :focus {
-    border: 1px solid #00b37e;
-  }
-`;
-
-const FormButton = styled.button`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 16px;
-  width: 100%;
-
-  background-color: #00875f;
-  border: none;
-  border-radius: 8px;
-
-  font-weight: 700;
-  font-size: 16px;
-  line-height: 19px;
-
-  color: #ffffff;
-
-  :hover {
-    background-color: #00b37e;
-  }
-`;
-
-const Posts = () => {
+const Posts: React.FC<PostsProps> = ({ posts }) => {
   return (
-    <PostContainer>
-      <PostTitle>
-        <PostOwner>
-          <PostOwnerAvatar>
-            <Image
-              src="https://images.unsplash.com/photo-1567532939604-b6b5b0db2604?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2787&q=80"
-              alt="Jane Cooper Profile"
-              height={40}
-              width={40}
-              style={{ borderRadius: '4px' }}
-            />
-          </PostOwnerAvatar>
-          <PostOwnerInfo>
-            <PostOwnerName>Jane Cooper</PostOwnerName>
-            <PostOwnerRole>Dev Front-end</PostOwnerRole>
-          </PostOwnerInfo>
-        </PostOwner>
-        <PostInfo>
-          <PostInfoTime>Posted 1hr ago</PostInfoTime>
-        </PostInfo>
-      </PostTitle>
-      <PostContent>
-        <PostContentText>Fala galeraa 👋</PostContentText>
-        <PostContentText>
-          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
-          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
-        </PostContentText>
-        <PostContentText>👉 jane.design/doctorcare</PostContentText>
-        <PostContentText>#novoprojeto #nlw #rocketseat</PostContentText>
-      </PostContent>
-      <Divider />
-      <Form>
-        <FormLabel>
-          <FormSpan>Deixe seu feedback</FormSpan>
-          <FormTextArea name="comment" id="comment" rows={5}></FormTextArea>
-        </FormLabel>
-        <FormButton>Publicar</FormButton>
-      </Form>
-    </PostContainer>
+    <>
+      {posts.map((post) => (
+        <PostItem key={Date.now()} {...post} />
+      ))}
+    </>
   );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ const Container = styled.div`
   flex-direction: column;
 
   min-height: 90vh;
-  max-width: 1280px;
+  max-width: 600px;
   padding: 20px;
   margin: 0 auto;
 `;
@@ -23,6 +23,35 @@ const Main = styled.main`
   gap: 20px;
 `;
 
+const posts = [
+  {
+    owner: {
+      name: 'Jane Cooper',
+      role: 'Dev Front-end',
+      avatarUrl:
+        'https://images.unsplash.com/photo-1567532939604-b6b5b0db2604?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2787&q=80',
+    },
+    createdAt: new Date(Date.now() - 36e5 - 1),
+    contentText:
+      'Fala galeraa 👋 \n Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀 \n 👉 jane.design/doctorcare \n #novoprojeto #nlw #rocketseat \n',
+    contentImage:
+      'https://images.unsplash.com/photo-1567532939604-b6b5b0db2604?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2787&q=80',
+  },
+  {
+    owner: {
+      name: 'Devon Lane',
+      role: 'Dev Front-end',
+      avatarUrl:
+        'https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80',
+    },
+    createdAt: new Date(Date.now() - 36e5 - 2),
+    contentText:
+      'Fala pessoal 👋 \n Finalmente finalizei meu novo site/portfólio. Foi um baita desafio criar todo o design e codar na unha, mas consegui 💪🏻 \n Acesse e deixe seu feedback 👉 devonlane.design \n #uiux #userexperience \n',
+    contentImage:
+      'https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80',
+  },
+];
+
 export default function Home() {
   return (
     <Container>
@@ -30,8 +59,7 @@ export default function Home() {
         <title>Feed</title>
       </Head>
       <Main>
-        <Posts />
-        <Posts />
+        <Posts posts={[...posts]} />
       </Main>
     </Container>
   );
